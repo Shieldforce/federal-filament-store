b8ec2c0c09f0241269d2c1990ce0e220544f40e2
Alexandre Ferreira <shieldforce2@gmail.com>
2025-11-30 17:52:28 -0300

    publish views providers

--- resources/views/layouts/external.blade.php
+++ resources/views/layouts/external.blade.php
@@ -3,27 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title ?? 'Loja' }}</title>
-
-    {{-- Vite (se seu app usa) --}}
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-
-    {{-- Estilos do Filament --}}
-    <x-filament::styles />
-
-    {{-- Estilos do Livewire --}}
     @livewireStyles
 </head>
-
-<body class="bg-gray-100 min-h-screen">
-
+<body class="bg-gray-100">
 <div class="container mx-auto p-4">
     {{ $slot }}
 </div>
-
-{{-- Scripts do Filament (inclui Alpine.js) --}}
-<x-filament::scripts />
-
-{{-- Scripts do Livewire --}}
 @livewireScripts
 </body>
 </html>
