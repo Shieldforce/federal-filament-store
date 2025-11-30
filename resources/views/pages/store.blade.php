54d28c153ad241e0ac383ea8e9aa89e35a2fd089
Alexandre Ferreira <shieldforce2@gmail.com>
2025-11-30 16:08:48 -0300

    publish views providers

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -27,7 +27,7 @@
                 <div class="bg-white dark:bg-gray-800 rounded-xl shadow-sm overflow-hidden border border-gray-200 dark:border-gray-700 hover:shadow-md transition">
 
                     <img
-                        src="{{ $product['image'] ?: asset('vendor/federal-filament-store/files/not-products-image.png') }}"
+                        src="{{ $product['image'] ?? asset('vendor/federal-filament-store/files/not-products-image.png') }}"
                         class="w-full h-48 object-cover"
                     />
 
