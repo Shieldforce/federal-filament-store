850099a5d5217c6dd1487b94c0d491989454ee3f
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-01 15:33:54 -0300

    css

--- resources/views/pages/store.blade.php
+++ resources/views/pages/store.blade.php
@@ -47,9 +47,8 @@
                         <div class="relative overflow-hidden">
                             <img
                                 src="{{ isset($product['image'])
-                                        ? "storage/{$product['image']}"
-                                        : asset('vendor/federal-filament-store/files/not-products-image.png')
-                                     }}"
+                                        ? "/storage/{$product['image']}"
+                                        : asset('vendor/federal-filament-store/files/not-products-image.png') }}"
                                 class="w-full h-48 object-cover transition-transform duration-300 group-hover:scale-105"
                             />
                         </div>
