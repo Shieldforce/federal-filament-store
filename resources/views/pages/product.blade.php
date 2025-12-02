44b1b06d88dee9353d9acd86e659f428e82ee933
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-02 20:48:33 -0300

    filtros

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -4,8 +4,8 @@
         {{-- SIDEBAR --}}
         <div class="md:w-[480px] lg:w-[520px] xl:w-[560px] md:flex-none pl-6 config-product-ec">
             <x-filament::section class="!max-w-[480px] w-full !rounded-none">
-                <h1 style="text-align: center;">
-                    {{ isset($this->product['price'])
+                <h1 style="text-align: center;font-size: 16pt;color: darkblue;">
+                    Preço: {{ isset($this->product['price'])
                     ? number_format($this->product['price'], 2, ',', '.') :
                     null }}
                 </h1>
