178d7d580206eba50e6421c9e95c8f2ba9912e76
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-03 14:23:38 -0300

    preço

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -54,10 +54,6 @@
         <div class="flex-1 pr-6">
             <div class="gap-6">
                 <div
-                    {{--x-data="{
-                        selected: 0,
-                        images: @js($this->images)
-                    }"--}}
                     x-data="{
                         selected: 0,
                         images: @js($this->images),
@@ -83,21 +79,39 @@
                     {{-- BOTÃO ESQUERDA --}}
                     <button
                         @click="selected = selected > 0 ? selected - 1 : images.length - 1"
-                        class="absolute inset-y-0 left-0 px-2 py-[25%] w-10 group hover:bg-gray-900/50 cursor-pointer"
+                        class="
+                            absolute top-1/2 -translate-y-1/2 left-4
+                            bg-black/40 hover:bg-black/70
+                            text-white w-10 h-10 rounded-full
+                            flex items-center justify-center
+                            shadow-lg backdrop-blur
+                            transition cursor-pointer
+                       "
                     >
-                        <span class="hidden group-hover:block text-white text-2xl">
-                            &larr;
-                        </span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M15 19l-7-7 7-7"/>
+                        </svg>
                     </button>
 
                     {{-- BOTÃO DIREITA --}}
                     <button
                         @click="selected = selected < images.length - 1 ? selected + 1 : 0"
-                        class="absolute inset-y-0 right-0 px-2 py-[25%] w-10 group hover:bg-gray-900/50 cursor-pointer"
-                    >
-                        <span class="hidden group-hover:block text-white text-2xl">
-                            &rarr;
-                        </span>
+                        class="
+                                absolute top-1/2 -translate-y-1/2 right-4
+                                bg-black/40 hover:bg-black/70
+                                text-white w-10 h-10 rounded-full
+                                flex items-center justify-center
+                                shadow-lg backdrop-blur
+                                transition cursor-pointer
+                           "
+                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
+                             viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                  d="M9 5l7 7-7 7"/>
+                        </svg>
                     </button>
 
                     {{-- ÍCONES INDICADORES --}}
