d048f4a83660166b5f1000d68ccefbbf51906416
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-04 11:25:52 -0300

    pedidos

--- config/federal-filament-store.php
+++ config/federal-filament-store.php
@@ -1,8 +1,10 @@
 <?php
 
+use Illuminate\Database\Eloquent\Model;
+
 return [
-    'sidebar_group' => 'Loja',
-    'products_callback' => [],
+    'sidebar_group'       => 'Loja',
+    'products_callback'   => [],
     'categories_callback' => [],
-    'clients_callback' => [],
+    'client_callback'     => Model::class,
 ];
