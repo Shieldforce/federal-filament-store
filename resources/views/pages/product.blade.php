02cfa8b5046a8744421991645f337dbd83bb3186
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-19 08:38:13 -0300

    produtos

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -6,14 +6,15 @@
             <x-filament::section class="!max-w-[480px] w-full !rounded-none">
                 <h1 style="font-size: 16pt;">
                     Total do Produto: {{ isset($this->totalPrice)
-                    ? "R$" . number_format($this->totalPrice, 2, ',', '.') :
-                    null }}
+                        ? "R$" . number_format($this->totalPrice, 2, ',', '.')
+                        : null }}
                 </h1>
+
                 <br>
                 <hr>
                 <br>
-                <form wire:submit="submit">
 
+                <form wire:submit="submit">
                     {{ $this->form }}
 
                     <br>
@@ -44,42 +45,52 @@
                         </x-filament::button>
 
                     </div>
-
                 </form>
-
             </x-filament::section>
-
         </div>
 
+        {{-- CONTEÚDO PRINCIPAL --}}
         <div class="flex-1 pr-6">
             <div class="gap-6">
+
+                {{-- SLIDER --}}
                 <div
                     x-data="{
                         selected: 0,
                         images: @js($this->images),
                         interval: null,
                         init() {
-                            // Troca automática a cada 10 segundos
                             this.interval = setInterval(() => {
-                                this.selected = this.selected < this.images.length - 1 ? this.selected + 1 : 0
+                                this.selected = this.selected < this.images.length - 1
+                                    ? this.selected + 1
+                                    : 0
                             }, 10000);
                         }
                     }"
                     class="relative w-full"
                 >
 
-                    {{-- IMAGEM --}}
-                    <img
-                        style="width: 100%;/*height: 400px;*/"
-                        class="h-64 w-full object-cover object-center rounded-xl shadow-lg"
-                        :src="images[selected]"
-                        alt=""
-                    />
+                    {{-- CONTAINER DA IMAGEM (ALTURA MÁX 400px, SEM CORTE) --}}
+                    <div
+                        class="relative w-full h-[300px] sm:h-[350px] lg:h-[400px]
+                               max-h-[400px]
+                               flex items-center justify-center
+                               bg-gray-100 dark:bg-gray-900
+                               overflow-hidden rounded-xl shadow-lg"
+                    >
+                        <img
+                            :src="images[selected]"
+                            alt=""
+                            class="max-w-full max-h-full object-contain
+                                   transition-opacity duration-500"
+                        />
+                    </div>
 
                     {{-- BOTÃO ESQUERDA --}}
                     <button
                         @click="selected = selected > 0 ? selected - 1 : images.length - 1"
-                        class="absolute inset-y-0 left-0 px-2 py-[25%] w-10 group hover:bg-gray-900/50 cursor-pointer"
+                        class="absolute inset-y-0 left-0 px-2 py-[25%] w-10
+                               group hover:bg-gray-900/50 cursor-pointer"
                     >
                         <span class="group-hover:block text-white text-3xl">
                             &larr;
@@ -89,14 +100,15 @@
                     {{-- BOTÃO DIREITA --}}
                     <button
                         @click="selected = selected < images.length - 1 ? selected + 1 : 0"
-                        class="absolute inset-y-0 right-0 px-2 py-[25%] w-10 group hover:bg-gray-900/50 cursor-pointer"
+                        class="absolute inset-y-0 right-0 px-2 py-[25%] w-10
+                               group hover:bg-gray-900/50 cursor-pointer"
                     >
                         <span class="group-hover:block text-white text-3xl">
                             &rarr;
                         </span>
                     </button>
 
-                    {{-- ÍCONES INDICADORES --}}
+                    {{-- INDICADORES --}}
                     <div class="absolute bottom-3 w-full flex justify-center gap-2">
                         <template x-for="(image, index) in images" :key="index">
                             <button
@@ -110,20 +122,16 @@
                         </template>
                     </div>
 
-                    <br>
-                    <br>
+                    <br><br>
                     <hr>
-                    <br>
-                    <br>
+                    <br><br>
 
                     {!! $this->product['description'] ?? '' !!}
-
                 </div>
 
             </div>
         </div>
     </div>
-
 </x-filament::page>
 
 @push('scripts')
@@ -132,12 +140,14 @@
         async function updateCartBadge() {
             const res = await fetch('/admin/cart-count');
             const count = await res.json();
-            const badge = document.querySelector('.fi-topbar-item a[href="/admin/ffs-cart"] .fi-badge .truncate');
-            if(badge) badge.textContent = count;
+            const badge = document.querySelector(
+                '.fi-topbar-item a[href="/admin/ffs-cart"] .fi-badge .truncate'
+            );
+            if (badge) badge.textContent = count;
         }
 
         window.addEventListener('redirect-after-delay', redirectAfterDelay);
-        async function redirectAfterDelay() {
+        function redirectAfterDelay() {
             setTimeout(() => {
                 window.location.href = "/admin/ffs-store";
             }, 30000);
