2b44b056bd57ff02a6f80a89c7d32b7d05f16c07
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-19 09:23:20 -0300

    produtos

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -10,40 +10,30 @@
                 <h1 style="font-size: 16pt;">
                     Total do carrinho: R$ {{ number_format($this->totalPrice, 2, ",", ".") }}
                 </h1>
+
                 <br>
                 <hr>
                 <br>
-                <form wire:submit="submit">
 
+                <form wire:submit="submit">
                     {{ $this->form }}
 
                     <br>
                     <hr>
 
                     <div class="flex flex-col mt-4 gap-4 w-full">
-                        {{-- FINALIZAR COMPRA --}}
-                        {{--<x-filament::button
-                            color="success"
-                            icon="heroicon-o-check-circle"
-                            class="w-full py-3 text-center"
-                            type="submit"
-                        >
-                            Ir Para checkout
-                        </x-filament::button>--}}
 
                         <x-filament::button
                             color="success"
                             icon="heroicon-o-check-circle"
                             class="w-full py-3 text-center"
                             type="submit"
-
                             wire:loading.class="opacity-50 cursor-not-allowed"
                             wire:loading.attr="disabled"
                             wire:target="submit"
                         >
                             <span wire:loading wire:target="submit">
-                                {{--<x-filament::loading-indicator class="w-5 h-5" />--}}
-                                 Processando...
+                                Processando...
                             </span>
 
                             <span wire:loading.remove wire:target="submit">
@@ -52,13 +42,11 @@
                         </x-filament::button>
 
                     </div>
-
                 </form>
-
             </x-filament::section>
-
         </div>
 
+        {{-- LISTA DE ITENS --}}
         <div class="flex-1 pr-6">
             <div class="gap-6">
 
@@ -66,16 +54,18 @@
                     @forelse($this->items as $item)
                         <div class="flex gap-4 p-4 rounded-xl border bg-white shadow-sm">
 
-                            {{-- IMAGEM --}}
-                            <div class="w-24 h-24 flex-none bg-gray-100 rounded-xl overflow-hidden">
+                            {{-- IMAGEM (SEM CORTE) --}}
+                            <div
+                                class="w-[200px] h-[130px] flex-none
+                                       flex items-center justify-center
+                                       bg-gray-100 rounded-xl overflow-hidden"
+                            >
                                 <img
-                                    src="{{
-                                        isset($item['data_product']['image'])
+                                    src="{{ isset($item['data_product']['image'])
                                         ? asset("storage/{$item['data_product']['image']}")
                                         : asset('vendor/federal-filament-store/files/not-products-image.png') }}"
                                     alt="{{ $item['name'] }}"
-                                    class="w-full h-full object-cover"
-                                    style="width: 200px;height: 130px;"
+                                    class="max-w-full max-h-full object-contain"
                                 >
                             </div>
 
@@ -102,7 +92,9 @@
                                             class="w-8 h-8 flex items-center justify-center border rounded-full hover:bg-gray-100"
                                         >–</button>
 
-                                        <span class="font-semibold text-lg">{{ $item['amount'] }}</span>
+                                        <span class="font-semibold text-lg">
+                                            {{ $item['amount'] }}
+                                        </span>
 
                                         <button
                                             wire:click="increaseQty('{{ $item['uuid'] }}')"
@@ -135,13 +127,7 @@
                     @endforelse
                 </div>
 
-
             </div>
         </div>
     </div>
-
 </x-filament::page>
-
-@push('scripts')
-
-@endpush
