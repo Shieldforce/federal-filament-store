3bd6a22c68f058914a874d0f3072ea894e761782
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-21 14:45:09 -0300

    users

--- src/Pages/FederalFilamentCartPage.php
+++ src/Pages/FederalFilamentCartPage.php
@@ -580,9 +580,6 @@ class FederalFilamentCartPage extends Page implements HasForms
                     "order_id" => $order->id,
                 ],
                 [
-                    'creator_id'         => DB::table("users")
-                                              ->where("email", "admin@admin.com")
-                                              ->first()->id,
                     'name'               => "Pagamento de carrinho de compras: {$data['cart_id']}",
                     'necessary'          => 1,
                     'type'               => TypeTransactionEnum::input->value,
