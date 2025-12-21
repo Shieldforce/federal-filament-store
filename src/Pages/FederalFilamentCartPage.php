b7c1e5ea89bcbff3da973bb92258226ebb9219d6
Alexandre Ferreira <shieldforce2@gmail.com>
2025-12-21 14:38:21 -0300

    users

--- src/Pages/FederalFilamentCartPage.php
+++ src/Pages/FederalFilamentCartPage.php
@@ -200,7 +200,7 @@ class FederalFilamentCartPage extends Page implements HasForms
     public
     function submit()
     {
-        DB::beginTransaction();
+        //DB::beginTransaction();
 
         try {
             $data = $this->form->getState();
@@ -292,9 +292,9 @@ class FederalFilamentCartPage extends Page implements HasForms
 
             $this->processCheckout($transaction, $isUser, $data);
 
-            DB::commit();
+            //DB::commit();
         } catch (Throwable $throwable) {
-            DB::rollBack();
+            //DB::rollBack();
 
             Notification::make()
                         ->danger()
